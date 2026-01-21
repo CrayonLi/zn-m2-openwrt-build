5767d2380c4293a4feb17d922224ca9dbca80d91
CrayonLi <84754186+CrayonLi@users.noreply.github.com>
2026-01-22 03:19:03 +0800

    Update diy-part2.sh

--- scripts/zn-m2/diy-part2.sh
+++ scripts/zn-m2/diy-part2.sh
@@ -9,10 +9,18 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+#更新所有feeds
+./scripts/feeds update -a
+#
 #优先安装 passwall 源
 # ./scripts/feeds install -a -f -p passwall_packages
 # ./scripts/feeds install -a -f -p passwall_luci
 #
+rm -rf feeds/packages/net/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/packages/net/openclash
+#
+./scripts/feeds install luci-app-openclash
 ./scripts/feeds install -p kenzok8 adguardhome
 ./scripts/feeds install -p kenzok8 luci-app-adguardhome
-#./scripts/feeds install -p kenzok8 luci-app-openclash
+
