faadfe13cee35ce9dac0f76d2296beed039bfa8a
CrayonLi <84754186+CrayonLi@users.noreply.github.com>
2026-01-21 23:45:47 +0800

    Update diy-part2.sh

--- scripts/zn-m2/diy-part2.sh
+++ scripts/zn-m2/diy-part2.sh
@@ -13,6 +13,6 @@
 # ./scripts/feeds install -a -f -p passwall_packages
 # ./scripts/feeds install -a -f -p passwall_luci
 #
-./scripts/feeds install -a -f -p adguardhome
-./scripts/feeds install -a -f -p luci-app-adguardhome
-./scripts/feeds install -a -f -p luci-app-openclash
+./scripts/feeds install -p kenzok8 adguardhome
+./scripts/feeds install -p kenzok8 luci-app-adguardhome
+./scripts/feeds install -p kenzok8 luci-app-openclash
