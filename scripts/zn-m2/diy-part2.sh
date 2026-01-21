157cf84e6c817324a88c84680e6f4a391e2d73c8
CrayonLi <84754186+CrayonLi@users.noreply.github.com>
2026-01-22 04:05:28 +0800

    Fix paths and update installation for openclash
    Removed outdated paths for luci-app-openclash and updated installation commands.

--- scripts/zn-m2/diy-part2.sh
+++ scripts/zn-m2/diy-part2.sh
@@ -9,16 +9,11 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-#更新所有feeds
-./scripts/feeds update -a
-#
 #优先安装 passwall 源
 # ./scripts/feeds install -a -f -p passwall_packages
 # ./scripts/feeds install -a -f -p passwall_luci
 #
-rm -rf feeds/packages/net/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/packages/net/openclash
 #
 ./scripts/feeds install luci-app-openclash
 ./scripts/feeds install -p kenzok8 adguardhome
